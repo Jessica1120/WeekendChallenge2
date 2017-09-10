24f50c7b5fbdd2e47e90187b0e19892152c236aa
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-09 19:01:54 -0500

    added body parser stuff

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //requires
+var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 //var moduleName if needed
@@ -7,17 +8,29 @@ var path = require('path');
 var app = express();
 var port = 3000
 
-var peopleList = [] //array of people sent from client side.
+var peopleList = []//array of people sent from client side.
 
 app.use(express.static('public'));// tells express to use this server path to find images etc.
 
-app.listen(port, function() {           //spins up server
-    console.log('server is up on', port);
- }) //end spin up
+app.use(bodyParser.urlencoded({extended: true})); // tell express to use body parser for objects
 
- //res.sendFile(path.resolve('./public/index.html'));
+app.get('/', function(res,req){
+     console.log('path')
+    res.sendFile(path.resolve('./Public/index.html'));
+ }); //I think this is for when a client first lands on a page.
 
- app.get('/personlist', function(req, res){
+app.get('/personlist', function(req, res){
      console.log('in personlist route');
      res.send(peopleList); // sends array to client.js when GET is requested
  })
+
+app.post('/personlist', function(req, res) {
+     var person = req.body.person;
+     console.log('post person route', person);
+    peopleList.push(person);
+    res.sendStatus(201);
+ });
+
+ app.listen(port, function() {  //spins up server
+    console.log('server is up on', port);
+ }) //end spin up
