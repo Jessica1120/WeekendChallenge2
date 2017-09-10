23c25476eca7789854020c772d72c40cc0a5444b
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-09 20:01:14 -0500

    working but null array return

--- Public/scripts/client.js
+++ Public/scripts/client.js
@@ -1,5 +1,6 @@
 function Ready() {
     $('#add').on('click', addPerson)
+    getPersonList();
  //function to request info from server
 };
 
@@ -8,20 +9,21 @@ function addPerson() {
     var nametoAdd = $('#name').val();
     var facttoAdd = $('#fact').val();
     console.log(nametoAdd, facttoAdd)
-    $('.person').val(''); //clears input form
-
+    
     //create object literal to hold data to send to server
     var person = {
         name: nametoAdd,
         fact: facttoAdd,
     };   
+    $('.person').val(''); //clears input form
     //post request for person list to send to server
     $.ajax({
        type: 'POST',
         url:  '/personlist',
         data:  person, 
         success: function(response) {
-        console.log(response);   
+        console.log(response); 
+        getPersonList();  
         }
     }); //ajax function for posting inside event listener function becuase POST will be sent on button click.
 }
@@ -32,7 +34,11 @@ function getPersonList() {
         type:   'GET',
         url:    '/personlist',
         success: function(response) {
+            $('#return').empty(); // empties contents of return section before updating array
             console.log(response);
+            for(var i = 1; i <response.length; i++) {
+                $('#return').append('<p>' + response[i] + '</p>'); //appends array to cleared section
+            }
         }   
     })
 }
--- server.js
+++ server.js
@@ -8,28 +8,27 @@ var path = require('path');
 var app = express();
 var port = 3000
 
-var peopleList = []//array of people sent from client side.
+var peopleList = [];//array of people sent from client side.
 
 app.use(express.static('public'));// tells express to use this server path to find images etc.
 
 app.use(bodyParser.urlencoded({extended: true})); // tell express to use body parser for objects
 
 app.get('/', function(res,req){
-     console.log('path')
-    res.sendFile(path.resolve('./Public/index.html'));
- }); //I think this is for when a client first lands on a page.
+     res.sendFile(path.resolve('./Public/index.html'));
+    }); //index route - I think this is for when a client first lands on a page.
 
-app.get('/personlist', function(req, res){
-     console.log('in personlist route');
+  app.get('/personlist', function(req, res){
+      console.log('in personlist route');
      res.send(peopleList); // sends array to client.js when GET is requested
- })
+   });
 
 app.post('/personlist', function(req, res) {
      var person = req.body.person;
      console.log('post person route', person);
     peopleList.push(person);
     res.sendStatus(201);
- });
+ }); // pushes the new person input object from client into array
 
  app.listen(port, function() {  //spins up server
     console.log('server is up on', port);
